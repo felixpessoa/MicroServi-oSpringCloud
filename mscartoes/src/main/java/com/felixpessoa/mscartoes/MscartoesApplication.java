a3ec4c6d547f88af4489c038728fa999bedb0ba8
felixpessoa <felixpessoa90@gmail.com>
2022-08-13 17:20:56 -0300

    Criado mscartoes e configurado o microservice de cartoes

---
+++ mscartoes/src/main/java/com/felixpessoa/mscartoes/MscartoesApplication.java
@@ -0,0 +1,15 @@
+package com.felixpessoa.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MscartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscartoesApplication.class, args);
+	}
+
+}
